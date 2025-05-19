7f07c680081b977e6f1fe3b3763d994d84995043
winnieKM <winniemorris254@example.com>
2025-05-19 08:42:26 +0300

    commit changes to settings.py and urls.py

--- alx_travel_app/settings.py
+++ alx_travel_app/settings.py
@@ -26,9 +26,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+      # Added apps
+    'rest_framework',
+    'corsheaders',
+    'drf_yasg',
+    'listings',
 ]
 
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- alx_travel_app/urls.py
+++ alx_travel_app/urls.py
@@ -20,3 +20,10 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('listings/', include('listings.urls')),  # If listings has urls.py
+]
